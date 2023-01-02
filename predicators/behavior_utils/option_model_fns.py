550f3762fc0f8d84d102f377f845e4cd62c90e36
Willie McClinton <wbm3@mit.edu>
2023-01-02 05:54:13 -0500

    fixe

--- predicators/behavior_utils/option_model_fns.py
+++ predicators/behavior_utils/option_model_fns.py
@@ -214,8 +214,11 @@ def create_place_option_model(
                         else:
                             assert isinstance(obj, (BRBody, BRHand, BREye))
                             obj_name = "agent"
-                        obj_type = _type_name_to_type[type_name]
-                        objs_under.add(Object(obj_name, obj_type))
+                        if type_name in _type_name_to_type:
+                            obj_type = _type_name_to_type[type_name]
+                            objs_under.add(Object(obj_name, obj_type))
+                        else:
+                            continue
         if len(objs_under) != 0:
             raise utils.EnvironmentFailure("collision",
                                            {"offending_objects": objs_under})
