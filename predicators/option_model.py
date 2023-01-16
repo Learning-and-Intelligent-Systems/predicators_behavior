d3a2885adabfe5cd01efaea7fd4cf54acd922ee9
Nishanth Kumar <nishanth.kumar20@gmail.com>
2023-01-15 22:08:27 -0500

    gnn option policy 456-458 reruns with fix

--- predicators/option_model.py
+++ predicators/option_model.py
@@ -146,9 +146,12 @@ class _BehaviorOptionModel(_OptionModelBase):
                     env.current_ig_state_to_state(
                         save_state=False,
                         use_test_scene=env.task_instance_id >= 10)):
-                load_checkpoint_state(state, env, reset=True)
+                if CFG.approach == "gnn_option_policy":
+                    reset = False
+                else:
+                    reset = True
+                load_checkpoint_state(state, env, reset=reset)
         option.memory["model_controller"](state, env.igibson_behavior_env)
-        # next_state = env.current_ig_state_to_state()
         next_state = env.current_ig_state_to_state(
             use_test_scene=env.task_instance_id >= 10)
         plan, _ = option.memory["planner_result"]
