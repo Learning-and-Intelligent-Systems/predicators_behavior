6bab445de976318807825bcf93cf6692b93727ee
Willie McClinton <wbm3@mit.edu>
2023-01-20 03:56:41 -0500

    added nsrt parser

--- run_count_nsrts.py
+++ run_count_nsrts.py
@@ -5,6 +5,8 @@ import re
 paths_to_approaches = ["/home/wbm3/Dropbox (MIT)/predicators_ignore_effs_ijcai23/experiment_data/behavior_domains/saved_approaches/*/*.NSRTs", 
     "/home/wbm3/Dropbox (MIT)/predicators_ignore_effs_ijcai23/experiment_data/aaai_domains/saved_approaches/*.NSRTs"]
 print("Running...")
+print()
+results = {"pnad_search": [],  "cluster_and_search": [], "cluster_and_intersect": [], "pred_error": []}
 for path in paths_to_approaches:
     for filename in glob.glob(path):
         exp_name = filename.split("__")[-1].split(".")[0]
@@ -12,7 +14,22 @@ for path in paths_to_approaches:
             with open(filename, 'rb') as f:
                 data = pkl.load(f)
                 nsrt_names = re.findall(r"\bNSRT-\w+", str(data))
-            print(exp_name, len(nsrt_names))
+            print(exp_name)
+            if "behavior" in filename and "sorting-books" in exp_name:
+                if "pnad_search" in exp_name:
+                    results["pnad_search"].append(len(nsrt_names))
+                if "cluster_and_search" in exp_name:
+                    results["cluster_and_search"].append(len(nsrt_names))
+                if "cluster_and_intersect" in exp_name:
+                    results["cluster_and_intersect"].append(len(nsrt_names))
+                if "pred_error" in exp_name:
+                    results["pred_error"].append(len(nsrt_names))
+                
         except Exception as e:
-            print(exp_name, e)
-            import ipdb; ipdb.set_trace()
+            # if "50demo" in exp_name:
+            #     print(filename, exp_name)
+            pass
+import numpy as np
+for key, val in results.items():
+    print(key, len(val), np.mean(val), np.std(val))
+import ipdb; ipdb.set_trace()
